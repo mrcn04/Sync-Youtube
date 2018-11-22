b2a5634410c32f131bf12df333e7fc7a1e8d4688
mrcn04 <ocengiz97@hotmail.com>
2018-11-22 03:54:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Sync Youtube
+
+*This is an early development Synced Youtube web app.*
+
+*I used Socket.io as web socket for TCP connection between server and client*
+
+It basicly let you to watch youtube videos synced with the people who is on the
+website. There is play and pause buttons and progress bar which synced with everyone.
+
+As addition, I added a little chatbox which takes your username when the page loads.
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
+
+```sh
+$ git clone https://github.com/mrcn04/Sync-Youtube.git
+$ cd Sync-Youtube
+$ npm install
+$ npm start or node app.js
+```
+
+Your app should now be running on [localhost:4000](http://localhost:4000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+
+### Documentation
+
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
---
+++ app.js
@@ -0,0 +1,60 @@
+// vars and consts
+const express = require('express');
+let app = express();
+const port = 4000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+// App setup
+app.set("view engine", "ejs");
+app.use(express.static("css"));  // Look up for css folder
+app.use(express.static("socket.io"));  // Look up for socket.io folder
+app.use(express.static(__dirname + '/public')); // Look up everything includes non folders
+
+// Routes
+// Home
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+// No Content page
+app.get("*", function(req, res){
+    res.status(403);
+    res.send("<h1> oppps </h1>");
+});
+
+// Port
+http.listen(port, function(){
+    console.log('listening on *: ' + port);
+  });
+
+// Socket setup
+io.on('connection', function(socket){
+    socket.on('event', function(msg){
+        console.log(msg);
+    }); 
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+    // username
+    socket.on('new user', function(un){
+        var newUser = un;
+        console.log(newUser + ' connected');
+        io.emit('connection2', "Welcome to Ömür's Chatbox " + newUser);  
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+            io.emit('disconnection', newUser + ' disconnected.');
+          });
+    });
+
+    // User connections
+    console.log('User ' + socket.id + ' Connected');
+    socket.on('disconnect', function(){
+        console.log('User ' + socket.id + ' Disconnected');
+    });
+    
+});
